193fa9196ec910adb9035cbeb4c0ff5d2444e02b
Artjom Islyamov <pixeltom@yandex.ru>
2021-05-31 14:45:04 +0300

    Конструктор

--- src/main/java/ru/geekbrains/Artjom_Islyamov/Employee.java
+++ src/main/java/ru/geekbrains/Artjom_Islyamov/Employee.java
@@ -1,5 +1,23 @@
 package ru.geekbrains.Artjom_Islyamov;
 
 public class Employee {
+    private final String fullName;
+    private final String post;
+    private final String eMail;
+    private final Long phoneNumber;
+    private final int salary;
+    private final int age;
 
+    public Employee(String fullName, String post, String eMail, Long phoneNumber, int salary, int age) {
+        this.fullName = fullName;
+        this.post = post;
+        this.eMail = eMail;
+        this.phoneNumber = phoneNumber;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public void showInfo() {
+        System.out.printf("%nfullName = %s, post = %s, eMail = %s, phoneNumber = %d, salary = %d, age = %d%n", fullName, post, eMail, phoneNumber, salary, age);
+    }
 }
---
+++ src/main/java/ru/geekbrains/Artjom_Islyamov/Person.java
@@ -0,0 +1,10 @@
+package ru.geekbrains.Artjom_Islyamov;
+
+public class Person {
+    public static void main(String[] args) {
+    Employee first = new Employee("Петров Иван Иванович","Директор дирекции",
+            "iIP@yandex.ru", 892594792051L,100000,45);
+            first.showInfo();
+
+    }
+}
