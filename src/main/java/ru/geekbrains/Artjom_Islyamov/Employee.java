1c0579927e2350eb36ca6f9eb1fa3b64062622bc
Artjom Islyamov <pixeltom@yandex.ru>
2021-05-31 16:01:39 +0300

    All Tasks

--- src/main/java/ru/geekbrains/Artjom_Islyamov/Employee.java
+++ src/main/java/ru/geekbrains/Artjom_Islyamov/Employee.java
@@ -1,12 +1,15 @@
 package ru.geekbrains.Artjom_Islyamov;
 
+import javax.jws.Oneway;
+
 public class Employee {
-    private final String fullName;
-    private final String post;
-    private final String eMail;
-    private final Long phoneNumber;
-    private final int salary;
-    private final int age;
+    private String fullName;
+    private String post;
+    private String eMail;
+    private Long phoneNumber;
+    private int salary;
+    private int age;
+
 
     public Employee(String fullName, String post, String eMail, Long phoneNumber, int salary, int age) {
         this.fullName = fullName;
@@ -17,7 +20,56 @@ public class Employee {
         this.age = age;
     }
 
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getPost() {
+        return post;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public Long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setPost(String post) {
+        this.post = post;
+    }
+
+    public void seteMail(String eMail) {
+        this.eMail = eMail;
+    }
+
+    public void setPhoneNumber(Long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     public void showInfo() {
         System.out.printf("%nfullName = %s, post = %s, eMail = %s, phoneNumber = %d, salary = %d, age = %d%n", fullName, post, eMail, phoneNumber, salary, age);
     }
+
 }
--- src/main/java/ru/geekbrains/Artjom_Islyamov/Person.java
+++ src/main/java/ru/geekbrains/Artjom_Islyamov/Person.java
@@ -2,9 +2,27 @@ package ru.geekbrains.Artjom_Islyamov;
 
 public class Person {
     public static void main(String[] args) {
-    Employee first = new Employee("Петров Иван Иванович","Директор дирекции",
-            "iIP@yandex.ru", 892594792051L,100000,45);
-            first.showInfo();
+        Employee testEmployee = new Employee("Петров Иван Иванович", "Директор дирекции",
+                "iIP@yandex.ru", 892594792051L, 100000, 45);
+        testEmployee.showInfo();//проверка
+        Employee[] persArray = new Employee[5];
 
+        persArray[0] = new Employee("Степанов Евгений Валентинович", "Директор департамента",
+                "StepByStep@icloud.com", 892594792051L, 254030, 39);
+        persArray[1] = new Employee("Иванов Дмитрий Феофанович", "старший инженер",
+                "IvanFeoFun@mail.ru", 891564792051L, 90000, 42);
+        persArray[2] = new Employee("Казаков Владимир Викторович", "Менеджер проектов",
+                "pupsik@yandex.ru", 890784762011L, 80000, 47);
+        persArray[3] = new Employee("Железнов Андрей Владимирович", "Генеральный директор",
+                "IronMan@yandex.ru", 892594792051L, 200000, 30);
+        persArray[4] = new Employee("Конаков Василий Сергеевич", "оператор ЧПУ",
+                "KonyaK@yandex.ru", 837594772851L, 70000, 38);
+
+        for (Employee first : persArray) {
+            if (first.getAge() > 40) {
+                first.showInfo();
+            }
+
+        }
     }
 }
